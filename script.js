b7f039bbf49560506088315e0b69c34a6997a866
Reiax98 <99645216+Reiax98@users.noreply.github.com>
2022-02-15 21:10:34 +0100

    Update script.js

--- script.js
+++ script.js
@@ -10,3 +10,14 @@ function gritalo(string) {
 }
 
 console.log(gritalo(sentence))
+
+var listacompra = ["banana", "fresa", "granada"]
+
+var path_meat = ["Pollo", "ternera", "hamburgesa"]
+var path_dairy = ["Yogurt", "Leche", "Queso"]
+var path = { seccionA: "Está en la Sección A", seccionB: "Está en la Sección B"}
+
+var tercerGato = { nombre: "garfield", apellido: "gato", direccion: "Casa de John Bonachón" }
+
+console.log(path_meat, path.seccionA)
+console.log(path_dairy, path.seccionB)
