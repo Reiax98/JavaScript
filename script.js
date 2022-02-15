34bc4b304c7d420e12a021a84ef9a838ad1c1280
Reiax98 <99645216+Reiax98@users.noreply.github.com>
2022-02-15 21:10:57 +0100

    Update script.js

--- script.js
+++ script.js
@@ -17,7 +17,5 @@ var path_meat = ["Pollo", "ternera", "hamburgesa"]
 var path_dairy = ["Yogurt", "Leche", "Queso"]
 var path = { seccionA: "Está en la Sección A", seccionB: "Está en la Sección B"}
 
-var tercerGato = { nombre: "garfield", apellido: "gato", direccion: "Casa de John Bonachón" }
-
 console.log(path_meat, path.seccionA)
 console.log(path_dairy, path.seccionB)
