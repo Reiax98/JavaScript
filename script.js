8230a4b3364e1ba370c1b20b5f6576abf3be1fef
Reiax98 <99645216+Reiax98@users.noreply.github.com>
2022-02-15 21:31:49 +0100

    Update script.js

--- script.js
+++ script.js
@@ -19,3 +19,16 @@ var path = { seccionA: "Está en la Sección A", seccionB: "Está en la Sección
 
 console.log(path_meat, path.seccionA)
 console.log(path_dairy, path.seccionB)
+
+var job = [
+  {
+    fecha: "20/02/2022",
+    inicio: "Recibimos nuestros roles"
+  },
+  {
+    fecha: "21/03/2022",
+    entrega: "La documentación de kanban"
+  }
+]
+
+console.log(job)
